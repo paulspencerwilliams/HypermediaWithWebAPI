063c616c6c863c17c5e874b809d9b1c3923c43da
Paul Williams <paul@paulswilliams.me.uk>
2013-07-01 16:12:54 +0100

    now tests following link from api entry point which currently is not implemented

--- AcceptanceTests/APIProxy.cs
+++ AcceptanceTests/APIProxy.cs
@@ -6,26 +6,34 @@ namespace AcceptanceTests
 {
     public class ApiProxy
     {
+        private readonly ApiFormat _format;
         private Resource _resource;
+        private string _baseURI;
 
         public enum ApiFormat { Json, Xml}
         public ApiProxy(ApiFormat format)
         {
+            _format = format;
+            _baseURI = "http://localhost";
+            _resource = PerformRequest(format,  "/api");
+        }
+
+        private Resource PerformRequest(ApiFormat format, string uri)
+        {
             using (var client = new HttpClient())
             {
-                var response = client.GetAsync("http://localhost/api");
+                var response = client.GetAsync(_baseURI + uri);
                 if (response.Result.IsSuccessStatusCode)
                 {
                     var responseString = response.Result.Content.ReadAsStringAsync().Result;
                     var jsonValue = JsonValue.Parse(responseString);
-                    _resource = new Resource(jsonValue, format);
+                    return new Resource(jsonValue, format);
                 }
                 else
                 {
                     string message = String.Format("Request failed with code ({0}) and message '{1}'",
                                                    new object[]
-                                                       {(int)response.Result.StatusCode, response.Result.ReasonPhrase});
-                ;
+                                                       {(int) response.Result.StatusCode, response.Result.ReasonPhrase});
                     throw new Exception(message);
                 }
             }
@@ -46,7 +54,9 @@ namespace AcceptanceTests
 
         public void FollowLink(string rel)
         {
-            
+            string uri = (string) _resource.JsonValue["_links"]["self"]["href"];
+            _resource = PerformRequest(_format, uri );
+
         }
 
         public class Resource
