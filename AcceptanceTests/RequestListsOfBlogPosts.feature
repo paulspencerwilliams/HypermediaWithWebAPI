3e632588a7bb3b000b777022082cf9f2a79bd981
Paul Williams <paul@paulswilliams.me.uk>
2013-07-01 13:18:26 +0100

    listing blog posts in json format scenario specced with empty steps

---
+++ AcceptanceTests/RequestListsOfBlogPosts.feature
@@ -0,0 +1,10 @@
+﻿Feature: Request a list of blog posts
+	In order to present a list of blog posts
+	As an API consumer
+	I want to be able to request a list of blog posts in JSON HAL & XML HAL formats
+
+Scenario: Request a list of blog posts in JSON format
+	Given I'm at the API entry point
+	And I specify "application/hal+json" on request headers
+	When I follow the link to a list of blog posts
+	Then I will receive a list of blog posts in JSON / HAL format including links to each blog post
---
+++ AcceptanceTests/Steps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests
+{
+    [Binding]
+    public class Steps
+    {
+        [Given(@"I'm at the API entry point")]
+        public void GivenIMAtTheAPIEntryPoint()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I specify ""(.*)"" on request headers")]
+        public void GivenISpecifyOnRequestHeaders(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I follow the link to a list of blog posts")]
+        public void WhenIFollowTheLinkToAListOfBlogPosts()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I will receive a list of blog posts in JSON / HAL format including links to each blog post")]
+        public void ThenIWillReceiveAListOfBlogPostsInJSONHALFormatIncludingLinksToEachBlogPost()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
